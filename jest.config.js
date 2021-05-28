c5e21ce8208efe69fba9eb539ef2abb14e330920
John MacKenzie <rjmackenzieii@gmail.com>
2021-05-28 10:56:26 -0400

    updated  with  to make tests pass

--- jest.config.js
+++ jest.config.js
@@ -12,4 +12,5 @@ module.exports = {
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/config/jest/assetMock.js',
   },
+  testEnvironment: 'jsdom',
 };
